f6753e767d07ece7e86cd25fdacdd6771520cbeb
lekhachanh <lekhachanh123@gmail.com>
2019-08-14 08:34:57 +0700

    first commit

---
+++ src/main/java/com/codegym/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.codegym;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    //Cấu hình này chỉ đăng ký bộ lọc springSecurityFilterChain
+    //cho mỗi URL cho ứng dụng của bạn.
+}
